6e213c82abbfb39c70913c61e3ea4ad22ce65cf5
Charlie <u202120841@upc.edu.pe>
2024-11-01 18:57:59 -0500

    fix: fix login screen, configure main screen

--- lib/account/presentation/screens/login_screen.dart
+++ lib/account/presentation/screens/login_screen.dart
@@ -1,186 +1,178 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:miam_flutter/account/application/bloc_or_cubit/login_cubit.dart';
+import 'package:miam_flutter/MainScreen.dart'; // Importar MonitoringScreen
 
-class BandConfigurationScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _rememberMe = false;
+  bool _obscurePassword = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Band Configuration"),
+        title: Text("Log In"),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            _buildBandInfoCard(),
-            _buildPatientInfoCard(),
-            _buildCaregiverInfoCard(),
-            _buildRelativeInfoCard(),
-            _buildResetButton(),
-            SizedBox(height: 20),
-            _buildSaveButton(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildBandInfoCard() {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "ID: 1       Status: Synchronized",
-              style: TextStyle(fontWeight: FontWeight.bold),
+            Icon(
+              Icons.login,
+              size: 50,
             ),
-            SizedBox(height: 8),
-            Text("Battery: 80%"),
-            Text("Last Sync: 2024-09-18 18:05"),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                "Sync now",
-                style: TextStyle(color: Colors.blue),
+            SizedBox(height: 20),
+            Text(
+              'Welcome!',
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Log in to your account',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: () {
+                // Aquí puedes implementar la lógica para Google Sign In
+              },
+              icon: Icon(Icons.g_mobiledata),
+              label: Text('Sign in with Google'),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(double.infinity, 50),
               ),
             ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildPatientInfoCard() {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Patient",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            SizedBox(height: 20),
+            Text('OR', style: TextStyle(color: Colors.grey)),
+            SizedBox(height: 20),
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
             ),
-            SizedBox(height: 8),
-            Text("Name: Carlos"),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildCaregiverInfoCard() {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Caregiver",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Text("Assigned to: Carolina Suarez"),
-            SizedBox(height: 16),
-            Text("Alerts:", style: TextStyle(fontWeight: FontWeight.bold)),
-            CheckboxListTile(
-              title: Text("Alert if temperature is below"),
-              value: false,
-              onChanged: (value) {},
-            ),
-            CheckboxListTile(
-              title: Text("Alert if pulse is below"),
-              value: false,
-              onChanged: (value) {},
-            ),
-            CheckboxListTile(
-              title: Text("Alert me if fall risk is high"),
-              value: false,
-              onChanged: (value) {},
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildRelativeInfoCard() {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Relative",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Related to: Carolina Suarez"),
-                TextButton.icon(
-                  onPressed: () {},
-                  icon: Icon(Icons.delete, color: Colors.red),
-                  label: Text(
-                    "Remove relative",
-                    style: TextStyle(color: Colors.red),
+            SizedBox(height: 20),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: _obscurePassword,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _obscurePassword ? Icons.visibility : Icons.visibility_off,
                   ),
+                  onPressed: () {
+                    setState(() {
+                      _obscurePassword = !_obscurePassword;
+                    });
+                  },
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {
+                  // Implementar la lógica para "Forgot password"
+                },
+                child: Text('Forgot password?'),
+              ),
+            ),
+            Row(
+              children: [
+                Checkbox(
+                  value: _rememberMe,
+                  onChanged: (value) {
+                    setState(() {
+                      _rememberMe = value!;
+                    });
+                  },
+                ),
+                Text('Remember me'),
+              ],
+            ),
+            SizedBox(height: 20),
+
+            // BlocConsumer para escuchar el estado del LoginCubit
+            BlocConsumer<LoginCubit, LoginState>(
+              listener: (context, state) {
+                if (state is LoginFailure) {
+                  // Mostrar un mensaje de error si el login falla
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(state.error)),
+                  );
+                }
+                if (state is LoginSuccess) {
+                  // Mostrar mensaje de éxito o navegar a la siguiente pantalla
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Login Successful')),
+                  );
+
+                  // Navegar a la pantalla MonitoringScreen
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MainScreen(),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                if (state is LoginLoading) {
+                  return CircularProgressIndicator();
+                }
+                return ElevatedButton(
+                  onPressed: () {
+                    final email = _emailController.text;
+                    final password = _passwordController.text;
+
+                    // Validar campos antes de enviar los datos
+                    if (email.isEmpty || password.isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Please fill in all fields')),
+                      );
+                    } else {
+                      // Llamar a la función de login del LoginCubit
+                      context.read<LoginCubit>().login(email, password);
+                    }
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('LOGIN'),
+                      SizedBox(width: 10),
+                      Icon(Icons.arrow_forward),
+                    ],
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 50),
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Don't have an account?"),
+                TextButton(
+                  onPressed: () {
+                    // Navegar a la pantalla de registro
+                  },
+                  child: Text('Register'),
                 ),
               ],
             ),
-            SizedBox(height: 16),
-            Text("Alerts:", style: TextStyle(fontWeight: FontWeight.bold)),
-            CheckboxListTile(
-              title: Text("Alert if temperature is below"),
-              value: false,
-              onChanged: (value) {},
-            ),
-            CheckboxListTile(
-              title: Text("Alert if pulse is below"),
-              value: false,
-              onChanged: (value) {},
-            ),
-            CheckboxListTile(
-              title: Text("Alert me if fall risk is high"),
-              value: false,
-              onChanged: (value) {},
-            ),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildResetButton() {
-    return Center(
-      child: TextButton.icon(
-        onPressed: () {},
-        icon: Icon(Icons.delete, color: Colors.red),
-        label: Text(
-          "Reset band",
-          style: TextStyle(color: Colors.red),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSaveButton() {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          // Acción para guardar la configuración
-        },
-        child: Text("Save"),
-        style: ElevatedButton.styleFrom(
-          minimumSize: Size(150, 50),
         ),
       ),
     );

1eea3a2b682a9131662ca12248b017f0c47156ee
Charlie <u202120841@upc.edu.pe>
2024-11-20 22:37:24 -0500

    fix: monitoring screen for viatal signal

--- lib/metrics/presentation/screens/monitoring_screen.dart
+++ lib/metrics/presentation/screens/monitoring_screen.dart
@@ -15,26 +15,6 @@ class MonitoringScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Alerts Section
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "Alerts",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-            ),
-            _buildAlertCard(
-              context,
-              title: "High risk of falling for María Carrillo",
-              description: "Immediate attention required",
-              color: Colors.redAccent,
-            ),
-            _buildAlertCard(
-              context,
-              title: "High temperature recorded for José Díaz",
-              description: "Current temperature: 38.5°C",
-              color: Colors.orangeAccent,
-            ),
 
             // Total Patients Section
             SizedBox(height: 20),
@@ -55,45 +35,11 @@ class MonitoringScreen extends StatelessWidget {
               ),
             ),
 
-            // General Health Status Section
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "General Health Status",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
-            _buildHealthStatus(),
-
-            // Medication Section
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                "Medication",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-            ),
-            _buildMedicationCard(
-              context,
-              time: "15:00",
-              patient: "María Carrillo",
-              medication: "Losartan 50 mg - 1 pill",
-              caregiver: "Carolina Suarez",
-            ),
-            _buildMedicationCard(
-              context,
-              time: "22:00",
-              patient: "José Díaz",
-              medication: "Glibenclamide 5 mg - 1/2 pill",
-              caregiver: "Carolina Suarez",
-            ),
-
             // Caregivers Section
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Text(
-                "Caregivers",
+                "Patients",
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
@@ -135,59 +81,6 @@ class MonitoringScreen extends StatelessWidget {
       ),
     );
   }
-
-  Widget _buildAlertCard(BuildContext context, {required String title, required String description, required Color color}) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      color: color.withOpacity(0.1),
-      child: ListTile(
-        leading: Icon(Icons.warning, color: color, size: 40),
-        title: Text(
-          title,
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text(description),
-        trailing: Icon(Icons.more_vert),
-      ),
-    );
-  }
-
-  Widget _buildHealthStatus() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: [
-          _buildHealthStatusItem(
-              label: "Temperature", status: "Follow-Up Required", color: Colors.orange),
-          _buildHealthStatusItem(label: "Pulse", status: "All Stable", color: Colors.green),
-          _buildHealthStatusItem(label: "Fall Risk", status: "Follow-Up Required", color: Colors.red),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildHealthStatusItem({required String label, required String status, required Color color}) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundColor: color,
-      ),
-      title: Text(label),
-      subtitle: Text(status),
-    );
-  }
-
-  Widget _buildMedicationCard(BuildContext context, {required String time, required String patient, required String medication, required String caregiver}) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: ListTile(
-        leading: Text(time),
-        title: Text(patient),
-        subtitle: Text(medication),
-        trailing: Text("Caregiver: $caregiver"),
-      ),
-    );
-  }
-
   Widget _buildVitalSignsChart(BuildContext context, {required String title, required List<double> data}) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
